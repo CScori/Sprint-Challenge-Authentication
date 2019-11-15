17b21d87af50589940c44d053568ae93aaa068d5
CScori <49799016+CScori@users.noreply.github.com>
2019-11-15 11:09:39 -0600

    auth mids

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const crypt = require('bycryptjs')
+const jwt = require('jsonwebtoken')
+
 
 router.post('/register', (req, res) => {
   // implement registration
